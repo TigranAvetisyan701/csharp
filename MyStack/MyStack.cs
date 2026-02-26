d44c54a7009f8f8c0d5b38605dd463568a63cb49
Tigran <tigranavetisyan701@gmail.com>
2026-02-27 02:46:40 +0400

    test

---
+++ MyStack/MyStack.cs
@@ -0,0 +1,26 @@
+﻿using MyLinkedListLib;
+
+namespace StackLib
+{
+    public class Stack<T>
+    {
+        private MyLinkedList<T> items = new MyLinkedList<T>();
+
+        public void Push(T item)
+        {
+            items.AddFirst(item);
+        }
+
+        public T Pop()
+        {
+            T item = items.First();
+            items.RemoveFirst();
+            return item;
+        }
+
+        public T Peek()
+        {
+            return items.First();
+        }
+    }
+}
---
+++ Test/Program.cs
@@ -0,0 +1,39 @@
+﻿using MyLinkedListLib;
+using StackLib;
+
+Console.WriteLine("MyLinkedList");
+
+MyLinkedList<int> list = new MyLinkedList<int>();
+list.AddLast(1);
+list.AddLast(2);
+list.AddLast(3);
+list.AddFirst(0);
+
+Console.Write("List: ");
+foreach (var item in list)
+    Console.Write(item + " ");
+Console.WriteLine();
+
+Console.WriteLine("Contains 2: " + list.Contains(2));
+
+list.RemoveFirst();
+list.RemoveLast();
+Console.Write("After RemoveFirst and RemoveLast: ");
+foreach (var item in list)
+    Console.Write(item + " ");
+Console.WriteLine();
+
+list.Clear();
+Console.WriteLine("Count after Clear: " + list.Count);
+
+Console.WriteLine("\nStack");
+
+StackLib.Stack<string> stack = new StackLib.Stack<string>();
+stack.Push("first");
+stack.Push("second");
+stack.Push("third");
+
+Console.WriteLine("Peek: " + stack.Peek());
+Console.WriteLine("Pop: " + stack.Pop());
+Console.WriteLine("Pop: " + stack.Pop());
+Console.WriteLine("Peek after two pops: " + stack.Peek());
